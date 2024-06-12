822f1cec6457cd40cb85fe1b4aef22e8a84b7c46
rahul khadak <khadakritika@gmail.com>
2024-06-12 18:34:18 +0530

    my first code

---
+++ test/src/main/java/org/example/Student.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Student {
+    int age;
+
+    int rollno;
+
+    String studentid;
+
+    public void study() {
+
+    }
+
+    public void eat() {
+
+    }
+}
+
+
+class Mainclass {
+
+    public static void main(String args[]) {
+
+
+        Student student = new Student();
+        student.rollno = 90;
+    }
+}
+

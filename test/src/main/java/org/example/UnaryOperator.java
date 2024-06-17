92f4a3489f1f28987a5b229ce3a1361a77d5de02
rahul khadak <khadakritika@gmail.com>
2024-06-17 19:37:00 +0530

    unaryoperator

---
+++ test/src/main/java/org/example/UnaryOperator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+    public class UnaryOperator {
+        public static void main(String[] args){
+            int number = 10;
+            System.out.println(number);
+            System.out.println(number--);
+            System.out.println(number);
+            System.out.println(--number);
+            System.out.println(++number);
+        }
+
+    }
+
+}

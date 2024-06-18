c38887790847718d5599648dcb5cc04a93fd13e3
AnujShukla760 <shuklaanuj312@gmail.com>
2024-06-18 16:07:00 +0530

    diff class

--- test/src/main/java/org/example/Subtraction.java
+++ test/src/main/java/org/example/Subtraction.java
@@ -5,19 +5,28 @@ public class Subtraction {
     int number2;
     int subtract;
 
-    public void subtraction() {
+    public void subtraction(){
         subtract = number1 - number2;
         if (subtract<5) {
             System.out.println("The number is less than five");
+            System.out.println(subtract);
         } else if (subtract>5 && subtract<15) {
             System.out.println("The number is between 5 and 15");
+            System.out.println(subtract);
         } else if (subtract>10 && subtract<20) {
             System.out.println("The number is between 10 and 20");
+            System.out.println(subtract);
         } else {
             System.out.println("The number is greater than 20");
+            System.out.println(subtract);
+
         }
 
         }
+
+
+
+
     }
 
 
--- test/src/main/java/org/example/UnaryOperator.java
+++ test/src/main/java/org/example/UnaryOperator.java
@@ -1,15 +1,20 @@
 package org.example;
 
     public class UnaryOperator {
-        public static void main(String[] args){
+        public static void main(String[] args) {
             int number = 10;
             System.out.println(number);
             System.out.println(number--);
             System.out.println(number);
             System.out.println(--number);
             System.out.println(++number);
+
+
+            Subtraction s = new Subtraction();
+
+            s.number1 = 12;
+            s.number2 = 5;
+            s.subtraction();
         }
-
     }
-
 

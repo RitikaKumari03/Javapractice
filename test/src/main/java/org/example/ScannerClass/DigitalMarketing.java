f59439fd029260cad2e97e24b8a0649ccbb6fb7a
RitikaKumari03 <khadakritika@gmail.com>
2024-06-20 17:50:05 +0530

    scanner

--- test/src/main/java/org/example/ScannerClass/DigitalMarketing.java
+++ test/src/main/java/org/example/ScannerClass/DigitalMarketing.java
@@ -3,24 +3,24 @@ package org.example.ScannerClass;
 import java.util.Scanner;
 
 public class DigitalMarketing {
-    int number ;
+    int numberofsales;
     String sales;
     Boolean profit;
 
-    public void workfromhome(){
+    public void workfromhome() {
     }
-  public static void main(String []args){
-       DigitalMarketing d = new DigitalMarketing();
-        Scanner scanner=new Scanner(System.in);
-      System.out.println("enter the number");
-        String number= scanner.nextLine();
-        d.number = Integer.parseInt(number);
-      System.out.println("the number is " + d.number);
-      d.workfromhome();
 
-
-
-
-
+    public static void main(String[] args) {
+        DigitalMarketing d = new DigitalMarketing();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number");
+        String number = scanner.nextLine();
+        d.numberofsales = Integer.parseInt(number);
+        System.out.println("the number is " + d.numberofsales);
+        d.workfromhome();
     }
 }
+
+
+
+

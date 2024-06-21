d9e9f3b94268584ecadf10878c914de8f1473451
RitikaKumari03 <khadakritika@gmail.com>
2024-06-21 17:20:32 +0530

    whileLoop

---
+++ test/src/main/java/org/example/Looping/WhileLoopPractice.java
@@ -0,0 +1,29 @@
+package org.example.Looping;
+
+public class WhileLoopPractice {
+    int i = 0;
+
+    public void whileloop() {
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 10);
+    }
+
+    public static void main(String args[]) {
+        WhileLoopPractice w = new WhileLoopPractice();
+        w.whileloop();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- test/src/main/java/org/example/ScannerClass/DigitalMarketing.java
+++ test/src/main/java/org/example/ScannerClass/DigitalMarketing.java
@@ -14,6 +14,7 @@ public class DigitalMarketing {
         DigitalMarketing d = new DigitalMarketing();
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the number");
+
         String number = scanner.nextLine();
         d.numberofsales = Integer.parseInt(number);
         System.out.println("the number is " + d.numberofsales);
